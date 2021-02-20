09d563d2c80f66ab1d3fd95704f6f3e5d3904c08
Taichi Yamakawa <xirckey@gmail.com>
2021-02-20 17:43:56 +0900

    Add an example of SortedSet

---
+++ src/test/scala/collection/SortedSetSpec.scala
@@ -0,0 +1,62 @@
+package collection
+
+import testing.BaseSpec
+
+import scala.collection.mutable
+
+/** [[https://www.scala-lang.org/api/current/scala/collection/mutable/SortedSet.html SortedSet]]
+  *  - mutable
+  */
+final class SortedSetSpec extends BaseSpec {
+
+  "SortedSet" should {
+
+    "be initialized with 3 elements" in {
+      val s = mutable.SortedSet(1, 2, 3)
+      s(1) shouldBe true
+      s(2) shouldBe true
+      s(3) shouldBe true
+    }
+
+    "be added an element using +=" in {
+      val s = mutable.SortedSet(1, 2, 3)
+      s(4) shouldBe false
+      s += 4
+      s(4) shouldBe true
+    }
+
+    "be added elements using ++=" in {
+      val s = mutable.SortedSet(1, 2, 3)
+      s(4) shouldBe false
+      s(5) shouldBe false
+      s ++= Seq(4, 5)
+      s(4) shouldBe true
+      s(5) shouldBe true
+    }
+  }
+
+  "union between two sets" in {
+    val s1 = mutable.SortedSet(1, 2, 3)
+    val s2 = mutable.SortedSet(2, 3, 4)
+    val s = s1 | s2
+    s shouldBe mutable.Set(1, 2, 3, 4)
+    s shouldBe (s1 union s2)
+  }
+
+  "intersection between two sets" in {
+    val s1 = mutable.SortedSet(1, 2, 3)
+    val s2 = mutable.SortedSet(2, 3, 4)
+    val s = s1 & s2
+    s shouldBe mutable.Set(2, 3)
+    s shouldBe (s1 intersect s2)
+  }
+
+  "difference between two sets" in {
+    val s1 = mutable.SortedSet(1, 2, 3)
+    val s2 = mutable.SortedSet(2, 3, 4)
+    val s = s1 &~ s2
+    s shouldBe mutable.Set(1)
+    s shouldBe (s1 diff s2)
+  }
+
+}
