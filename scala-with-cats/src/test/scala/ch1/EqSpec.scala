ff2443ac0d562c874c134d380fe40c41bf815805
Taichi Yamakawa <xirckey@gmail.com>
2021-04-11 23:48:32 +0900

    Example: Eq

---
+++ scala-with-cats/src/test/scala/ch1/EqSpec.scala
@@ -0,0 +1,57 @@
+package ch1
+
+import cats.Eq
+import cats.instances.int._
+import cats.instances.long._
+import cats.instances.option._
+import cats.syntax.eq._
+import cats.syntax.option._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+import java.text.DateFormat
+import java.util.Date
+
+final class EqSpec extends AnyWordSpecLike with Matchers {
+
+  "equals int" in {
+
+    val eqInt = Eq[Int]
+
+    eqInt.eqv(123, 123) shouldBe true
+    eqInt.eqv(123, 124) shouldBe false
+
+  }
+
+  "use syntax equals" in {
+
+    (123 eqv 123) shouldBe true
+    (123 eqv 124) shouldBe false
+    (123 =!= 124) shouldBe true
+
+  }
+
+  "use option" in {
+
+    (Option(1) eqv None) shouldBe false
+    (1.some eqv None) shouldBe false
+    (1.some =!= None) shouldBe true
+
+  }
+
+  "compare custom types" in {
+
+    implicit val dateEq: Eq[Date] = Eq.instance { (date1, date2) =>
+      date1.getTime eqv date2.getTime
+    }
+
+    val fmt = DateFormat.getDateInstance
+    val x = fmt.parse("2021/04/11")
+    val y = fmt.parse("2021/04/12")
+
+    (x eqv x) shouldBe true
+    (x eqv y) shouldBe false
+
+  }
+
+}
