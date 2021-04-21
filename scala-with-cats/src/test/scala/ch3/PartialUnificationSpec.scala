f4ccf369277008bb8d9e4f01fe64711e40c6f5a2
Taichi Yamakawa <xirckey@gmail.com>
2021-04-21 21:46:36 +0900

    Example: Partial Unification

---
+++ scala-with-cats/src/test/scala/ch3/PartialUnificationSpec.scala
@@ -0,0 +1,28 @@
+package ch3
+
+import cats.instances.function._
+import cats.syntax.functor._
+import cats.syntax.contravariant._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+final class PartialUnificationSpec extends AnyWordSpecLike with Matchers {
+
+  val func1: Int => Double = _.toDouble
+  val func2: Double => Double = _ * 2
+  val func3: Int => Double = func1.map(func2)
+
+  func3(1) shouldBe 2
+
+  val func3a: Int => Double = a => func2(func1(a))
+  val func3b: Int => Double = func2.compose(func1)
+
+  func3a(1) shouldBe 2
+  func3b(1) shouldBe 2
+
+  type <=[B, A] = A => B
+  val func2b: Double <= Double = func2
+  val func3c: Int => Double = func2b.contramap(func1)
+  func3c(1) shouldBe 2
+
+}
