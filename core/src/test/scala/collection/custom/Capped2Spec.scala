8002722b787ccaaf186d0643de88423db1e523ae
Taichi Yamakawa <xirckey@gmail.com>
2021-04-21 22:50:46 +0900

    Example: Custom Iterable Collection (Capped2)

---
+++ core/src/test/scala/collection/custom/Capped2Spec.scala
@@ -0,0 +1,28 @@
+package collection.custom
+
+import testing.BaseSpec
+
+private final class Capped2Spec extends BaseSpec {
+
+  object Capped2 extends Capped2Factory(capacity = 4)
+
+  val c1 = Capped2(1, 2, 3)
+  c1 shouldBe a[Capped2[_]]
+  c1.toSeq shouldBe Seq(1, 2, 3)
+
+  val c2 = c1.take(2)
+  c2 shouldBe a[Capped2[_]]
+  c2.toSeq shouldBe Seq(1, 2)
+
+  val c3 = c1.filter(x => x % 2 == 1)
+  c3 shouldBe a[Capped2[_]]
+  c3.toSeq shouldBe Seq(1, 3)
+
+  val c4 = c1.map(x => x * x)
+  c4 shouldBe a[Capped2[_]]
+  c4.toSeq shouldBe Seq(1, 4, 9)
+
+  val c5 = List(1, 2, 3, 4, 5).to(Capped2)
+  c5 shouldBe a[Capped2[_]]
+
+}
