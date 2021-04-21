2c8e9479ca35eb13ace2ad69479b46e9167d6248
Taichi Yamakawa <xirckey@gmail.com>
2021-04-21 20:13:39 +0900

    Exercise: Contravariant Functors

---
+++ scala-with-cats/src/test/scala/ch3/ContravariantFunctorSpec.scala
@@ -0,0 +1,44 @@
+package ch3
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+final class ContravariantFunctorSpec extends AnyWordSpecLike with Matchers {
+
+  trait Printable[A] { self =>
+    def format(value: A): String
+    def contramap[B](f: B => A): Printable[B] = new Printable[B] {
+      override def format(value: B): String =
+        self.format(f(value))
+    }
+  }
+
+  def format[A](value: A)(implicit p: Printable[A]): String =
+    p.format(value)
+
+  implicit val stringPrintable: Printable[String] = new Printable[String] {
+    override def format(value: String): String = s"'$value'"
+  }
+  implicit val booleanPrintable: Printable[Boolean] = new Printable[Boolean] {
+    override def format(value: Boolean): String = if (value) "yes" else "no"
+  }
+
+  "printable" in {
+    format("hello") shouldBe "'hello'"
+    format(true) shouldBe "yes"
+  }
+
+  "contramap" in {
+
+    final case class Box[A](value: A)
+    implicit def stringBoxPrintable[A](implicit
+        ev: Printable[A]
+    ): Printable[Box[A]] =
+      ev.contramap(box => box.value)
+
+    format(Box("hello world")) shouldBe "'hello world'"
+    format(Box(true)) shouldBe "yes"
+
+  }
+
+}
