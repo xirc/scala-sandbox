39995ba245258e3f948e52c6d865e20f8f876347
Taichi Yamakawa <xirckey@gmail.com>
2021-04-24 20:39:34 +0900

    Exercise: Writer

--- scala-with-cats/src/test/scala/ch4/CatsWriterSpec.scala
+++ scala-with-cats/src/test/scala/ch4/CatsWriterSpec.scala
@@ -3,10 +3,18 @@ package ch4
 import cats.data.Writer
 import cats.instances.vector._
 import cats.syntax.all._
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 
-final class CatsWriterSpec extends AnyWordSpecLike with Matchers {
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+
+final class CatsWriterSpec
+    extends AnyWordSpecLike
+    with Matchers
+    with ScalaFutures {
 
   type Logged[A] = Writer[Vector[String], A]
 
@@ -65,6 +73,48 @@ final class CatsWriterSpec extends AnyWordSpecLike with Matchers {
 
     val w6 = w.swap
     w6.run shouldBe ((3, Vector("a", "b", "c")))
+
+  }
+
+  "exercise" in {
+
+    def slowly[A](body: => A): A = {
+      try body
+      finally Thread.sleep(100)
+    }
+
+    def factorial(n: Int): Logged[Int] = {
+      for {
+        ans <- slowly {
+          if (n == 0)
+            1.pure[Logged]
+          else
+            factorial(n - 1).map(n * _)
+        }
+        _ <- Vector(s"fact $n $ans").tell
+      } yield {
+        ans
+      }
+    }
+
+    val rs = Await.result(
+      Future.sequence(
+        Vector(
+          Future(factorial(5)),
+          Future(factorial(5))
+        )
+      ),
+      5.seconds
+    )
+
+    rs(0).run._1 shouldBe Vector(
+      "fact 0 1",
+      "fact 1 1",
+      "fact 2 2",
+      "fact 3 6",
+      "fact 4 24",
+      "fact 5 120"
+    )
 
   }
 
