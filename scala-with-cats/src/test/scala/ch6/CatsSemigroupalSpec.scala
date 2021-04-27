8701262e3db7681dbbe062e85a64f9e57e582cb2
Taichi Yamakawa <xirckey@gmail.com>
2021-04-27 19:20:11 +0900

    Example: Semigroupal

---
+++ scala-with-cats/src/test/scala/ch6/CatsSemigroupalSpec.scala
@@ -0,0 +1,51 @@
+package ch6
+
+import cats.Semigroupal
+import cats.instances.option._
+import cats.instances.function._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+final class CatsSemigroupalSpec extends AnyWordSpecLike with Matchers {
+
+  "option" in {
+
+    Semigroupal[Option].product(
+      Option(1),
+      Option("a")
+    ) shouldBe Option((1, "a"))
+
+    Semigroupal[Option].product(
+      Option(1),
+      None
+    ) shouldBe None
+
+    Semigroupal.tuple3(
+      Option(1),
+      Option("a"),
+      Option(true)
+    ) shouldBe Option((1, "a", true))
+
+    Semigroupal.tuple3(
+      Option(1),
+      Option("a"),
+      None
+    ) shouldBe None
+
+    Semigroupal.map2(
+      Option(1),
+      Option(2)
+    )(_ + _) shouldBe Option(3)
+
+  }
+
+  "function" in {
+
+    val f1: Int => Double = _.toDouble * 2
+    val f2: Int => Double = _.toDouble * 3
+    val f3: Int => Double = Semigroupal.map2(f1, f2)(_ + _)
+    f3(1) shouldBe 5.0
+
+  }
+
+}
