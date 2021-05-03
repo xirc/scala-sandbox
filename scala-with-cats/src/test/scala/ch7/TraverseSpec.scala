dec2293c8acd0a84b94d27047cd1fc2fca513cfc
Taichi Yamakawa <xirckey@gmail.com>
2021-05-03 16:35:38 +0900

    Exercise: Traverse

--- scala-with-cats/src/test/scala/ch7/TraverseSpec.scala
+++ scala-with-cats/src/test/scala/ch7/TraverseSpec.scala
@@ -1,7 +1,11 @@
 package ch7
 
 import cats.Applicative
+import cats.data.Validated
 import cats.instances.future._
+import cats.instances.list._
+import cats.instances.option._
+import cats.instances.vector._
 import cats.syntax.applicative._
 import cats.syntax.apply._
 import org.scalatest.concurrent.ScalaFutures
@@ -72,6 +76,56 @@ final class TraverseSpec
     times.futureValue shouldBe expectedUpTimes
 
     listTraverse(hostnames)(getUpTime).futureValue shouldBe expectedUpTimes
+
+  }
+
+  "Traversing with Vectors" in {
+
+    listSequence(List(Vector(1, 2), Vector(3, 4))) shouldBe
+      Vector(List(1, 3), List(1, 4), List(2, 3), List(2, 4))
+
+    listSequence(List(Vector(1, 2), Vector(3, 4), Vector(5, 6))) shouldBe
+      Vector(
+        List(1, 3, 5),
+        List(1, 3, 6),
+        List(1, 4, 5),
+        List(1, 4, 6),
+        List(2, 3, 5),
+        List(2, 3, 6),
+        List(2, 4, 5),
+        List(2, 4, 6)
+      )
+
+  }
+
+  "Traversing with Options" in {
+
+    def process(inputs: List[Int]): Option[List[Int]] =
+      listTraverse(inputs)(n => if (n % 2 == 0) Option(n) else None)
+
+    process(List(2, 4, 6)) shouldBe Option(List(2, 4, 6))
+    process(List(1, 2, 3)) shouldBe None
+
+  }
+
+  "Traversing with Validated" in {
+
+    type ErrorsOr[A] = Validated[List[String], A]
+
+    def process(inputs: List[Int]): ErrorsOr[List[Int]] =
+      listTraverse(inputs) { n =>
+        if (n % 2 == 0) {
+          Validated.valid(n)
+        } else {
+          Validated.invalid(List(s"$n is not even"))
+        }
+      }
+
+    process(List(2, 4, 6)) shouldBe
+      Validated.valid(List(2, 4, 6))
+
+    process(List(1, 2, 3)) shouldBe
+      Validated.invalid(List("1 is not even", "3 is not even"))
 
   }
 
