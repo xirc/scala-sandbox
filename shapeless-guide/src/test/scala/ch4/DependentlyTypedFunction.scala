ffb94c357cba99bd0dffb0772f3c931d05936e6d
Taichi Yamakawa <xirckey@gmail.com>
2021-05-11 20:11:46 +0900

    Example: Chaining dependent functions

--- shapeless-guide/src/test/scala/ch4/DependentlyTypedFunction.scala
+++ shapeless-guide/src/test/scala/ch4/DependentlyTypedFunction.scala
@@ -1,7 +1,7 @@
 package ch4
 
-import shapeless.{::, HList, HNil}
-import shapeless.ops.hlist.Last
+import shapeless.{::, Generic, HList, HNil}
+import shapeless.ops.hlist.{IsHCons, Last}
 import testing.BaseSpec
 
 object DependentlyTypedFunction {
@@ -45,6 +45,35 @@ final class DependentlyTypedFunction extends BaseSpec {
 
     second1("foo" :: 1 :: true :: HNil) shouldBe 1
     second2("baz" :: false :: 2 :: HNil) shouldBe false
+
+  }
+
+  "chain" in {
+
+    def lastField[A, Repr <: HList](input: A)(implicit
+        gen: Generic.Aux[A, Repr],
+        last: Last[Repr]
+    ): last.Out = last.apply(gen.to(input))
+
+    case class ClassA(x: Int)
+    case class ClassB(x: Int, y: Double)
+    case class ClassC(x: Int, y: Double, z: Boolean)
+    lastField(ClassA(1)) shouldBe 1
+    lastField(ClassB(1, 1.23)) shouldBe 1.23
+    lastField(ClassC(1, 1.23, false)) shouldBe false
+
+  }
+
+  "chain2" in {
+
+    def unwrap[A, Repr <: HList, H](input: A)(implicit
+        gen: Generic.Aux[A, Repr],
+        isHCons: IsHCons.Aux[Repr, H, HNil]
+    ): H = gen.to(input).head
+
+    case class ClassA(x: Int)
+
+    unwrap(ClassA(1)) shouldBe 1
 
   }
 
